5f166e965d44ed9b834b6550cfae9615ba08e4e4
Radoslaw Stepien <radoslaw.stepien@student.kdg.be>
2024-03-23 20:34:19 +0100

    TreasureFound_Fact works

--- python_code/treasure_found_fact.py
+++ python_code/treasure_found_fact.py
@@ -27,7 +27,7 @@ def insert_treasure_found_facts(cursor_dwh):
     cursor_dwh.execute('''
     INSERT INTO TreasureFound_Fact (TreasureID, WeatherID, DateSK, UserSK, SearchTime) 
     SELECT 
-        td.id,
+        (SELECT id FROM TreasureTypeDim WHERE difficulty = t.difficulty AND terrain = t.terrain AND size = s.container_size AND visibility = s.visibility),
         CASE 
             WHEN wh.amount_of_rain >= 0.1 THEN 0
             WHEN wh.amount_of_rain < 0.1 THEN 1
@@ -39,16 +39,13 @@ def insert_treasure_found_facts(cursor_dwh):
     FROM catchem.dbo.treasure_log tl
     JOIN Date_Dim dd ON dd.date = CONVERT(date, tl.log_time)
     JOIN UserDim ud ON ud.UserID = tl.hunter_id
-    JOIN TreasureTypeDim td ON td.id = tl.treasure_id
-    LEFT JOIN catchem.dbo.weather_history wh ON wh.log_time = DATEADD(hour, DATEDIFF(hour, 0, tl.log_time), 0)
-    WHERE tl.log_type = 2 AND td.id IS NOT NULL; 
+    JOIN catchem.dbo.treasure t ON t.id = tl.treasure_id
+    JOIN catchem.dbo.treasure_stages ts ON ts.treasure_id = t.id
+    JOIN catchem.dbo.stage s ON s.id = ts.stages_id
+    LEFT OUTER JOIN catchem.dbo.weather_history wh ON wh.log_time = DATEADD(hour, DATEDIFF(hour, 0, tl.log_time), 0)
+    WHERE tl.log_type = 2 AND s.id = (SELECT TOP 1 id FROM catchem.dbo.treasure_stages ts2 JOIN catchem.dbo.stage s2 ON s2.id = ts2.stages_id WHERE ts2.treasure_id = t.id ORDER BY s2.sequence_number desc); 
     ''')
     cursor_dwh.commit()
-
-
-
-
-
 
 
 def main():
