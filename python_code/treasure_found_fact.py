0ab1605d4fb69d624aa1113759b6340c77f1798b
Pamela Barbosa de Almeida <pamela.barbosadealmeida@student.kdg.be>
2024-03-23 17:57:32 +0100

    update fact table

--- python_code/treasure_found_fact.py
+++ python_code/treasure_found_fact.py
@@ -7,21 +7,25 @@ def create_treasure_found_fact_table(cursor_dwh):
     cursor_dwh.execute('''
     IF OBJECT_ID(N'TreasureFound_Fact', N'U') IS NULL 
     CREATE TABLE TreasureFound_Fact (
-    id INT PRIMARY KEY IDENTITY(1,1),
+    FactID INT PRIMARY KEY IDENTITY(1,1),
     TreasureID INT NOT NULL,
     WeatherID INT NOT NULL,
-    DateID INT NOT NULL,
+    DateSK INT NOT NULL,
     UserSK INT NOT NULL,
-    SearchTime DECIMAL(5, 2) NOT NULL
+    SearchTime DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (TreasureID) REFERENCES TreasureTypeDim(id),
+    FOREIGN KEY (WeatherID) REFERENCES Rain_Dim(id),
+    FOREIGN KEY (DateSK) REFERENCES Date_Dim(DateSK),
+    FOREIGN KEY (UserSK) REFERENCES UserDim(UserSK)
     )
     ''')
 
-    cursor_dwh.commit() 
+    cursor_dwh.commit()
 
 
-def insert_treasure_found_facts(cursor):
-    cursor.execute('''
-    INSERT INTO catchem_dwh.dbo.TreasureFound_Fact (TreasureID, WeatherID, DateID, UserSK, SearchTime) 
+def insert_treasure_found_facts(cursor_dwh):
+    cursor_dwh.execute('''
+    INSERT INTO TreasureFound_Fact (TreasureID, WeatherID, DateSK, UserSK, SearchTime) 
     SELECT 
         td.id,
         CASE 
@@ -29,29 +33,34 @@ def insert_treasure_found_facts(cursor):
             WHEN wh.amount_of_rain < 0.1 THEN 1
             ELSE 2
         END,
-        dd.id,
+        dd.DateSK,
         ud.UserSK,
-        DATEDIFF(ss, tl.log_time, tl.session_start)
+        DATEDIFF(ss, tl.session_start, tl.log_time)
     FROM catchem.dbo.treasure_log tl
-    JOIN catchem_dwh.dbo.Date_Dim dd ON dd.date = convert(date, tl.log_time)
-    JOIN catchem_dwh.dbo.UserDim ud ON ud.id = tl.hunter_id
-    JOIN catchem_dwh.dbo.TreasureDim td ON td.id = tl.treasure_id
-    LEFT OUTER JOIN catchem.dbo.weather_history wh ON wh.log_time = dateadd(hour, datediff(hour, 0, tl.log_time), 0)
-    WHERE tl.log_type = 2;
+    JOIN Date_Dim dd ON dd.date = CONVERT(date, tl.log_time)
+    JOIN UserDim ud ON ud.UserID = tl.hunter_id
+    JOIN TreasureTypeDim td ON td.id = tl.treasure_id
+    LEFT JOIN catchem.dbo.weather_history wh ON wh.log_time = DATEADD(hour, DATEDIFF(hour, 0, tl.log_time), 0)
+    WHERE tl.log_type = 2 AND td.id IS NOT NULL; 
     ''')
-    cursor.commit()
+    cursor_dwh.commit()
+
+
+
+
+
 
 
 def main():
     try:
         # Establish connections
         conn_dwh = dwh.establish_connection(SERVER, DATABASE_DWH, USERNAME, PASSWORD, DRIVER)
-        
+
         with conn_dwh.cursor() as cursor_dwh:
-            # Creature TreasureFoundFact table if it doesn't exist
+            # Create TreasureFoundFact table if it doesn't exist
             create_treasure_found_fact_table(cursor_dwh)
 
-            # Insert treasure found facts 
+            # Insert treasure found facts
             insert_treasure_found_facts(cursor_dwh)
 
         # Close connections
@@ -62,3 +71,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
